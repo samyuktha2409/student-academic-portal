5edafa3b73f381bdca313f726d95936f9744588a
samyuktha2409 <2024510006@student.annauniv.edu>
2026-06-27 11:10:40 +0530

    Create subjects.js

---
+++ js/subjects.js
@@ -0,0 +1,97 @@
+const semesters = {
+
+1: [
+ {code:"EN23C01", name:"Foundation English", credit:3},
+ {code:"MA23C01", name:"Matrices and Calculus", credit:4},
+ {code:"PH23C01", name:"Engineering Physics", credit:4},
+ {code:"ME23C01", name:"Engineering Drawing & 3D Modelling", credit:4},
+ {code:"EE23C02", name:"Fundamentals of Electrical and Electronics Engineering", credit:3},
+ {code:"ME23C04", name:"Makerspace", credit:3},
+ {code:"UC23H01", name:"Heritage of Tamils", credit:1},
+ {code:"NCC", name:"NCC/NSS/NSO/YRC", credit:0, exclude:true}
+],
+
+2: [
+ {code:"EN23C02", name:"Professional Communication", credit:3},
+ {code:"MA23C05", name:"Probability and Statistics", credit:4},
+ {code:"PH23C09", name:"Semiconductor Devices and Quantum Technologies", credit:3},
+ {code:"CY23C01", name:"Engineering Chemistry", credit:4},
+ {code:"AD23201", name:"Python Programming", credit:3},
+ {code:"AD23202", name:"Computer Organization", credit:3},
+ {code:"UC23H02", name:"Tamils and Technology", credit:1},
+ {code:"AUDIT1", name:"Audit Course I", credit:0, exclude:true}
+],
+
+3: [
+ {code:"MA23C04", name:"Discrete Mathematics", credit:4},
+ {code:"AD23301", name:"Object Oriented Programming and Data Structures", credit:5},
+ {code:"AD23302", name:"Computer Networks", credit:3},
+ {code:"AD23303", name:"Database Management Systems", credit:5},
+ {code:"AD23304", name:"Fundamentals of Data Science", credit:3},
+ {code:"AD23U01", name:"Standards – Artificial Intelligence", credit:1},
+ {code:"UC23U01", name:"Universal Human Values", credit:0, exclude:true}
+],
+
+4: [
+ {code:"MA23C03", name:"Linear Algebra and Numerical Methods", credit:4},
+ {code:"IT23C02", name:"Operating Systems", credit:4},
+ {code:"IT23C01", name:"Design and Analysis of Algorithms", credit:3},
+ {code:"AD23401", name:"Data Exploration and Visualization", credit:4},
+ {code:"AD23402", name:"Computer Vision", credit:4},
+ {code:"AD23403", name:"Distributed Systems and Cloud Computing", credit:3},
+ {code:"IOC1", name:"Industry Oriented Course I", credit:1},
+ {code:"AD23L01", name:"Self Learning Course I", credit:1},
+ {code:"SDC1", name:"Skill Development Course I", credit:2},
+ {code:"AUDIT2", name:"Audit Course II", credit:0, exclude:true}
+],
+
+5: [
+ {code:"AD23501", name:"Optimization Techniques", credit:4},
+ {code:"AD23502", name:"Machine Learning Methods", credit:4},
+ {code:"AD23503", name:"Artificial Intelligence", credit:4},
+ {code:"AD23504", name:"Big Data Analytics", credit:4},
+ {code:"PEC1", name:"Professional Elective I", credit:3},
+ {code:"SDC2", name:"Skill Development Course II", credit:2},
+ {code:"UC23E01", name:"Engineering Entrepreneurship Development", credit:3},
+ {code:"AD23505", name:"Societal Oriented Project", credit:1}
+],
+
+6: [
+ {code:"AD23601", name:"Deep Learning Techniques", credit:3},
+ {code:"AD23602", name:"Natural Language Processing", credit:3},
+ {code:"ETC1", name:"Emerging Technology Course I", credit:3},
+ {code:"PEC2", name:"Professional Elective II", credit:3},
+ {code:"PEC3", name:"Professional Elective III", credit:3},
+ {code:"OE1", name:"Open Elective I", credit:3},
+ {code:"SDC3", name:"Skill Development Course III", credit:2},
+ {code:"IOC2", name:"Industry Oriented Course II", credit:1},
+ {code:"AD23U02", name:"Perspectives of Sustainable Development – Information Technology", credit:3}
+],
+
+7: [
+ {code:"AD23701", name:"Machine Learning Operations", credit:3},
+ {code:"ETC2", name:"Emerging Technology Course II", credit:3},
+ {code:"PEC4", name:"Professional Elective IV", credit:3},
+ {code:"PEC5", name:"Professional Elective V", credit:3},
+ {code:"PEC6", name:"Professional Elective VI", credit:3},
+ {code:"OE2", name:"Open Elective II", credit:3},
+ {code:"IOC3", name:"Industry Oriented Course III", credit:1},
+ {code:"AD23702", name:"Software Development Project Laboratory", credit:2}
+],
+
+8: [
+ {code:"AD23801", name:"Project Work / Internship cum Project Work", credit:8}
+]
+
+};
+
+const semesterCredits = {
+  1:22,
+  2:21,
+  3:23,
+  4:26,
+  5:25,
+  6:24,
+  7:21,
+  8:8
+};
